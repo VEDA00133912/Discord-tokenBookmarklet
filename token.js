7f816ba2e3bf1ede4b632fbd82b5674537042983
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-08-07 15:37:44 +0900

    Update token.js

--- token.js
+++ token.js
@@ -3,22 +3,15 @@ javascript:(function() {
     [Math.random()],
     {},
     req => {
-      if (!req.c) return;
-      for (const m of Object.keys(req.c)
-        .map(x => req.c[x].exports)
-        .filter(x => x)) {
-        if (m.default && m.default.getToken !== undefined) {
-          return navigator.clipboard.writeText(m.default.getToken()).then(() => {
+      for (const m of Object.values(req.c).map(x => x.exports).filter(x => x)) {
+        const token = m.default?.getToken ?? m.getToken;
+        if (token) {
+          navigator.clipboard.writeText(token()).then(() => {
             alert('トークンがクリップボードにコピーされました！');
           });
-        }
-        if (m.getToken !== undefined) {
-          return navigator.clipboard.writeText(m.getToken()).then(() => {
-            alert('トークンがクリップボードにコピーされました！');
-          });
+          break;
         }
       }
     },
   ]);
 })();
-// https://github.com/aiko-chan-ai/discord.js-selfbot-v13
